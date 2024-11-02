251fddc784383b98cb1bf64dae12f8cd77975c68
Mehrsa <mehrsaref@gmail.com>
2024-11-02 12:31:48 +0330

    header is created

---
+++ src/router.js
@@ -0,0 +1,26 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+import CoachDetail from './pages/coaches/CoachDetail.vue';
+import CoachesList from './pages/coaches/CoachesList.vue';
+import CoachRegisteration from './pages/coaches/CoachRegisteration.vue';
+import ContactCoach from './pages/requests/ContactCoach.vue';
+import RequestsReceived from './pages/requests/RequestsReceived.vue';
+import NotFound from './pages/NotFound.vue';
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: '/', redirect: '/coaches/' },
+    { path: '/coaches', component: CoachesList },
+    {
+      path: '/coaches/:id',
+      component: CoachDetail,
+      children: [{ path: 'contact', component: ContactCoach }],
+    },
+    { path: '/register', component: CoachRegisteration },
+    { path: '/requests', component: RequestsReceived },
+    { path: '/:notFount(.*)', component: NotFound },
+  ],
+});
+
+export default router;
